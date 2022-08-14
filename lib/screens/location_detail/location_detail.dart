a06ca0fa524c75aec6a58bc5bf596d07d1f74aba
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-08-14 21:00:21 +0000

    some changes

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 
-class LocationDetail extends StatelessWidget
-{
+class LocationDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Hello World!'),
       ),
-      body: const Text(''),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              color: Colors.red,
+            ),
+            child: const Text('Some bull shit'),
+          ),
+
+          Container(
+            decoration: const BoxDecoration(
+              color: Colors.green,
+            ),
+            child: const Text('Some bull shit'),
+          ),
+
+          Container(
+            decoration: const BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: const Text('Some bull shit'),
+          ),
+        ],
+      ),
     );
   }
-
 }
